f8ecad2a7dbde8c2a4790a506726de15cbf5ed2a
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-03-02 16:23:02 +0300

    fix: fix launch app

--- bot/apps.py
+++ bot/apps.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from django.apps import AppConfig
 import telebot
 from django.conf import settings
@@ -6,11 +8,12 @@ from django.conf import settings
 class BotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bot'
-    webhook = "tgr835hvdf.loclx.io"
-    bot = telebot.TeleBot(settings.BOT_TOKEN)
+
+    bot = telebot.TeleBot(token=settings.BOT_TOKEN)
+    webhook = "jfwofryjht.loclx.io"
 
     def ready(self):
-
         self.bot.remove_webhook()
+        sleep(1)
         self.bot.set_webhook(self.webhook)
 
