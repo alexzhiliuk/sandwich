a2a7284608abe6cbd5257194e53721358fd5d901
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-03-20 13:15:28 +0300

    fix: remove stats from creating page

--- accounts/templatetags/accounts_tags.py
+++ accounts/templatetags/accounts_tags.py
@@ -11,11 +11,15 @@ register = template.Library()
 
 @register.simple_tag
 def get_stats_for_point(point, start_date=None, end_date=None):
+    if not point:
+        return
     orders = point.orders.all()
     return get_orders_stats_for_period(orders, start_date, end_date)
 
 
 @register.simple_tag
-def get_stats_for_owner(point, start_date=None, end_date=None):
-    orders = point.orders.all()
+def get_stats_for_owner(owner, start_date=None, end_date=None):
+    if not owner:
+        return
+    orders = owner.orders.all()
     return get_orders_stats_for_period(orders, start_date, end_date)
